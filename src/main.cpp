0d00e9ce4eca30f572f8439de40eb8764976c5d2
Mohannad Zeido <mohannad.zeido@gmail.com>
2019-04-10 08:41:26 +0100

    Added initial time setting button
    Cleaned up commnets

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,34 @@
-// Sample RFM69 sender/node sketch, with ACK and optional encryption, and Automatic Transmission Control
-// Sends periodic messages of increasing length to gateway (id=1)
-// It also looks for an onboard FLASH chip, if present
-// **********************************************************************************
-// Copyright Felix Rusu 2018, http://www.LowPowerLab.com/contact
-// **********************************************************************************
-// License
-// **********************************************************************************
-// This program is free software; you can redistribute it
-// and/or modify it under the terms of the GNU General
-// Public License as published by the Free Software
-// Foundation; either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will
-// be useful, but WITHOUT ANY WARRANTY; without even the
-// implied warranty of MERCHANTABILITY or FITNESS FOR A
-// PARTICULAR PURPOSE. See the GNU General Public
-// License for more details.
-//
-// Licence can be viewed at
-// http://www.gnu.org/licenses/gpl-3.0.txt
-//
-// Please maintain this license information along with authorship
-// and copyright notices in any redistribution of this code
-// **********************************************************************************
-#include <RFM69.h>         //get it here: https://www.github.com/lowpowerlab/rfm69
-#include <RFM69_ATC.h>     //get it here: https://www.github.com/lowpowerlab/rfm69
-#include <SPIFlash.h>      //get it here: https://www.github.com/lowpowerlab/spiflash
-#include <SPI.h>           //included with Arduino IDE install (www.arduino.cc)
+#include <RFM69.h>
+#include <RFM69_ATC.h>
+#include <SPIFlash.h>
+#include <SPI.h>
 
-//*********************************************************************************************
-//************ IMPORTANT SETTINGS - YOU MUST CHANGE/CONFIGURE TO FIT YOUR HARDWARE ************
-//*********************************************************************************************
 //******************Green Antenna*************************************************
 // #define NODEID        2    //must be unique for each node on same network (range up to 254, 255 is used for broadcast)
 // #define NETWORKID     100  //the same on all nodes that talk to each other (range up to 255)
 // #define GATEWAYID     1
 
-//****************White Antenna******************************
+//******************White Antenna*************************************************
 #define NODEID        1    //must be unique for each node on same network (range up to 254, 255 is used for broadcast)
 #define NETWORKID     100  //the same on all nodes that talk to each other (range up to 255)
 #define GATEWAYID     2 //todo change variable name to node 2
-//Match frequency to the hardware version of the radio on your Moteino (uncomment one):
+
 #define FREQUENCY   RF69_433MHZ
-//#define FREQUENCY   RF69_868MHZ
-// #define FREQUENCY     RF69_915MHZ
+
 #define ENCRYPTKEY    "sampleEncryptKey" //exactly the same 16 characters/bytes on all nodes!
 #define IS_RFM69HW_HCW  //uncomment only for RFM69HW/HCW! Leave out if you have RFM69W/CW!
-//*********************************************************************************************
-//Auto Transmission Control - dials down transmit power to save battery
-//Usually you do not need to always transmit at max output power
-//By reducing TX power even a little you save a significant amount of battery power
-//This setting enables this gateway to work with remote nodes that have ATC enabled to
-//dial their power down to only the required level (ATC_RSSI)
+
 #define ENABLE_ATC    //comment out this line to disable AUTO TRANSMISSION CONTROL
 #define ATC_RSSI      -80
 //*********************************************************************************************
 #define SERIAL_BAUD   115200
 
-#if defined (MOTEINO_M0) && defined(SERIAL_PORT_USBVIRTUAL)
-  #define Serial SERIAL_PORT_USBVIRTUAL // Required for Serial on Zero based boards
-#endif
-
 const int buttonPin = 7;     // the number of the pushbutton pin
 const int ledPin =  6;      // the number of the LED pin
 int buttonState = 0;
-char payload[] = "123 ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char buff[20];
 byte sendSize=0;
-boolean requestACK = false;
+int timesButtonPressed = 0;
 SPIFlash flash(SS_FLASHMEM, 0xEF30); //EF30 for 4mbit  Windbond chip (W25X40CL)
 
 #ifdef ENABLE_ATC
@@ -131,22 +91,50 @@ long lastPeriod = 0;
 void loop() {
   //process any serial input
   buttonState = digitalRead(buttonPin);
-
-  if(buttonState == HIGH){
-    Serial.print("Sending[");
-    Serial.print("msg");
-    Serial.print("]: ");
-      Serial.print("moty");
-    if (radio.sendWithRetry(GATEWAYID, "moty", 4)){
-      Serial.print(" ok!");
+  //todo implement millis so that code does not read false positives
+  if(digitalRead(buttonPin) == HIGH){
+    timesButtonPressed = timesButtonPressed + 15;
+    if(timesButtonPressed == 15){
+      Serial.print("Times Button Pressed: ");
+      Serial.println(timesButtonPressed);
+      //todo light 1 LED
+    }else if(timesButtonPressed == 30){
+      Serial.print("Times Button Pressed: ");
+      Serial.println(timesButtonPressed);
+      //todo light 2 LEDs
+    }else if(timesButtonPressed == 45){
+      Serial.print("Times Button Pressed: ");
+      Serial.println(timesButtonPressed);
+      //todo light 3 LEDs
+    }else if(timesButtonPressed == 60){
+      Serial.print("Times Button Pressed: ");
+      Serial.println(timesButtonPressed);
+      //todo light 4 LEDs
+    }else if(timesButtonPressed > 60){
+      timesButtonPressed = 0;
+      Serial.print("Times Button Pressed: ");
+      Serial.println(timesButtonPressed);
+      //todo turn off all LEDs
     }
-    else{
-       Serial.print(" nothing...");
-    }
-    Serial.println();
-    Blink(LED_BUILTIN,3);
-      delay(500);
+    delay(500);
   }
+  buttonState = 0;
+
+  // if(buttonState == HIGH){
+  //   Serial.print("Sending[");
+  //   Serial.print("msg");
+  //   Serial.print("]: ");
+  //     Serial.print("moty");
+  //   if (radio.sendWithRetry(GATEWAYID, "moty", 4)){
+  //     Serial.print(" ok!");
+  //   }
+  //   else{
+  //      Serial.print(" nothing...");
+  //   }
+  //   Serial.println();
+  //   Blink(LED_BUILTIN,3);
+  //     delay(500);
+  // }
 
   if (Serial.available() > 0)
   {
